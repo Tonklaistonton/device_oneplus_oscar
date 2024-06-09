f8492f619e40a6c40c05c7c920ddfbefd6542c08
Tonton <115675957+Tonklaistonton@users.noreply.github.com>
2024-06-09 12:49:50 +0700

    Update lineage_oscar.mk

--- lineage_oscar.mk
+++ lineage_oscar.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oscar device
 $(call inherit-product, device/oneplus/oscar/device.mk)
 
-# Inherit some common Derpfest stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_oscar
 PRODUCT_DEVICE := oscar
