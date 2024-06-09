6ea49d3eb87498ce9bcd7dba7c7b3e77c3e12b0f
Tonton <115675957+Tonklaistonton@users.noreply.github.com>
2024-06-09 23:16:05 +0700

    Update lineage_oscar.mk

--- lineage_oscar.mk
+++ lineage_oscar.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/oneplus/oscar/device.mk)
 # Inherit some common Evolution stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+#add evothink
+TARGET_DISABLE_EPPE := true
+
 PRODUCT_NAME := lineage_oscar
 PRODUCT_DEVICE := oscar
 PRODUCT_MANUFACTURER := OnePlus
